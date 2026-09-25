1807af85637222707f21d9dd82ed029b23b2ea25
Pratham <prathamgarg1502@gmail.com>
2026-09-25 15:57:57 +0000

    Implemented General Signal Counter that Counts the Initialilizing Signal sof the Machine

---
+++ C/Signal.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+
+// Global variable to count signal triggers
+volatile sig_atomic_t signal_count = 0;
+
+// TODO: Write your signal handler function here
+void handle_sigint(int sig) { 
+     
+	signal_count +=1 ;
+
+	if( signal_count < 3 ) {
+		
+		printf("CAUGHT SIGINT!! press ctrl+c %d more times to exit " , 3-signal_count) ;
+
+	}
+
+	else{
+
+		printf("\n Programm EXIT\n") ;
+		exit(0) ;
+
+	}
+
+}
+
+int main() {
+    
+    signal(SIGINT, handle_sigint) ;
+    printf("Program running (PID: %d). Try pressing Ctrl+C...\n", getpid());
+
+    while (1) {
+        sleep(1);
+    }
+
+    return 0;
+}
